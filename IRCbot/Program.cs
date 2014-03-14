effa49dc0fa9e7cc74fc92567279fb144453fc83
Jiri Korinek <korineczek@yahoo.com>
2014-03-14 13:18:36 +0100

    IRC bot files
    bot.cs is a unity script that connects to twitch.tv IRC channel and
    detects commands that are input in the chat to use them as game
    controls.
    
    IRCbot is the same bot without unity integration

---
+++ IRCbot/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ircclient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int port;
+            string buf, nick, owner, server, chan, pass, command;
+            char[] splitter = {':'};
+            System.Net.Sockets.TcpClient sock = new System.Net.Sockets.TcpClient();
+            System.IO.TextReader input;
+            System.IO.TextWriter output;
+
+            //Get nick, owner, server, port, and channel from user
+            pass = "oauth:fdu3pr6qgiqrd01o4kkiaa7crmv4t0c";
+            nick = "lightzors";
+            owner = "lightzors";
+            server = "irc.twitch.tv";
+            port = 6667;
+            chan = "#fifaralle";
+
+            //Connect to irc server and get input and output text streams from TcpClient.
+            sock.Connect(server, port);
+            if (!sock.Connected)
+            {
+                Console.WriteLine("Failed to connect!");
+                return;
+            }
+            input = new System.IO.StreamReader(sock.GetStream());
+            output = new System.IO.StreamWriter(sock.GetStream());
+
+            //Starting USER and NICK login commands 
+            output.Write(
+               "PASS "+ pass +"\r\n" + "USER " + nick + " 0 * :" + owner + "\r\n" +
+               "NICK " + nick + "\r\n"
+            );
+            output.Flush();
+
+            //Process each line received from irc server
+            for (buf = input.ReadLine(); ; buf = input.ReadLine())
+            {
+                command = buf.Split(splitter)[buf.Split(splitter).Length-1];
+                //Display received irc message
+                Console.WriteLine(buf);
+
+                switch (command)
+                {
+                    case "ping":
+                        Console.WriteLine("fuckyeah");
+                        break;
+                }
+
+                //Send pong reply to any ping messages
+                if (buf.StartsWith("PING")){
+                    Console.WriteLine("ping");}
+                    //{ output.Write(buf.Replace("PING", "PONG") + "\r\n"); output.Flush(); }
+                if (buf[0] != ':') continue;
+
+                /* IRC commands come in one of these formats:
+                 * :NICK!USER@HOST COMMAND ARGS ... :DATA\r\n
+                 * :SERVER COMAND ARGS ... :DATA\r\n
+                 */
+
+                //After server sends 001 command, we can set mode to bot and join a channel
+                if (buf.Split(' ')[1] == "001")
+                {
+                    output.Write(
+                       "MODE " + nick + " +B\r\n" +
+                       "JOIN " + chan + "\r\n"
+                    );
+                    output.Flush();
+                }
+            }
+        }
+    }
+}
---
+++ bot.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class bot : MonoBehaviour {
+        int port;
+        string buf, nick, owner, server, chan, pass, command;
+        char[] splitter = { ':' };
+        System.Net.Sockets.TcpClient sock = new System.Net.Sockets.TcpClient();
+        System.IO.TextReader input;
+        System.IO.TextWriter output;
+
+       
+	// Use this for initialization
+	void Awake () {
+       
+        //Get nick, owner, server, port, and channel from user
+        pass = "";
+        nick = "lightzors";
+        owner = "lightzors";
+        server = "irc.twitch.tv";
+        port = 6667;
+        chan = "#fifaralle";
+
+        //Connect to irc server and get input and output text streams from TcpClient.
+        sock.Connect(server, port);
+        if (!sock.Connected)
+        {
+            Debug.Log("Failed to connect!");
+        }
+        else
+        {
+            input = new System.IO.StreamReader(sock.GetStream());
+            output = new System.IO.StreamWriter(sock.GetStream());
+            output.Write(
+              "PASS " + pass + "\r\n" + "USER " + nick + " 0 * :" + owner + "\r\n" +
+              "NICK " + nick + "\r\n"
+           );
+            output.Flush();
+            StartCoroutine(Refresh());
+        }
+	
+	 //Starting USER and NICK login commands 
+           
+}
+	// Update is called once per frame
+    IEnumerator Refresh()
+    {
+        while (true)
+        {
+            buf = input.ReadLine();
+            command = buf.Split(splitter)[buf.Split(splitter).Length - 1];
+            //Display received irc message
+            Debug.Log(buf);
+
+            switch (command)
+            {
+                case "ping":
+                    Debug.Log("fuckyeah");
+                    break;
+            }
+            if (buf.Split(' ')[1] == "001")
+            {
+                output.Write(
+                   "MODE " + nick + " +B\r\n" +
+                   "JOIN " + chan + "\r\n"
+                );
+                output.Flush();
+            }
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+}
