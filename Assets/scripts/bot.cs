2d42903ee91605f781e31d4ba7b7ab2432db78df
Jiri Korinek <korineczek@yahoo.com>
2014-03-28 14:42:50 +0100

    Bot unity implementation + performance tweaks
    - Cleaning up generation code
    - Reduced number of spawning items in order to improve performance
    -Added some comments to generation script

---
+++ Assets/scripts/bot.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class bot : MonoBehaviour
+{
+    // Variable list
+    public string gameCommand;
+    int port;
+    string buf, nick, owner, server, chan, pass, command;
+    char[] splitter = { ':' };
+    System.Net.Sockets.TcpClient sock = new System.Net.Sockets.TcpClient();
+    System.IO.TextReader input;
+    System.IO.TextWriter output;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        //Get nick, owner, server, port, and channel from user
+        pass = "oauth:";
+        nick = "lightzors";
+        owner = "lightzors";
+        server = "irc.twitch.tv";
+        port = 6667;
+        chan = "#fifaralle";
+
+        //Connect to irc server and get input and output text streams from TcpClient.
+        sock.Connect(server, port);
+        if (!sock.Connected)
+        {
+            Debug.Log("Failed to connect!");
+        }
+        else
+        {
+            //Input credentials
+            input = new System.IO.StreamReader(sock.GetStream());
+            output = new System.IO.StreamWriter(sock.GetStream());
+            output.Write(
+              "PASS " + pass + "\r\n" + "USER " + nick + " 0 * :" + owner + "\r\n" +
+              "NICK " + nick + "\r\n"
+           );
+            output.Flush();
+            //Initialize IRC checking
+            StartCoroutine("Refresh");
+        }
+
+        //Starting USER and NICK login commands 
+
+    }
+    // Update is called once per frame
+    IEnumerator Refresh()
+    {
+        while (true)
+        {
+            //Split command line input to only display message
+            buf = input.ReadLine();
+            command = buf.Split(splitter)[buf.Split(splitter).Length - 1];
+            //Display received irc message
+            Debug.Log(buf);
+
+            //Filter messages
+            switch (command)
+            {
+                case "bomb":
+                    gameCommand = "bomb";
+                    break;
+                case "up":
+                    gameCommand = "up";
+                    break;
+                case "down":
+                    gameCommand = "down";
+                    break;
+                case "left":
+                    gameCommand = "left";
+                    break;
+                case "right":
+                    gameCommand = "right";
+                    break;
+
+                //Close IRC Stream
+                case "lolmotherfuckersyousuck":
+                    input.Close();
+                    output.Close();
+                    sock.Close();
+                    StopCoroutine("Refresh");
+                    break;
+            }
+            if (buf.Split(' ')[1] == "001")
+            {
+                output.Write(
+                   "MODE " + nick + " +B\r\n" +
+                   "JOIN " + chan + "\r\n"
+                );
+                output.Flush();
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+}
--- Assets/scripts/generateBOXESlol.cs
+++ Assets/scripts/generateBOXESlol.cs
@@ -1,125 +1,141 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class generateBOXESlol : MonoBehaviour {
-	
-	public Transform ragingboner;
-	public Transform camera;
-	public Transform crate;
-	public Transform wall;
-	public Transform player1;
-	public Transform player2;
-	public int maxw = 10;
-	public int maxh = 10;
-	private int[,] x;
-	private int y = 0;
-	public int numbercrate = 10;
-	private int number, spawnnumber;
+public class generateBOXESlol : MonoBehaviour
+{
+    //Variables
+    public Transform ragingboner;
+    public Transform camera;
+    public Transform crate;
+    public Transform wall;
+    public Transform player1;
+    public Transform player2;
+    public int maxw = 10;
+    public int maxh = 10;
+    private int[,] x;
+    private int y = 0;
+    public int numbercrate = 10;
+    private int number, spawnnumber;
 
-	
 
-	// Use this for initialization
-	void Start () {
-		
-		spawnnumber = Random.Range(0,4);
-		camera.transform.position = new Vector3(maxh/2,maxh+5,maxw/2);
-		x = new int [maxh,maxw];
-		for(int i= 0; i<=maxh-1; i++)
-			for(int k= 0; k<=maxw-1; k++){
-			 	if(i==0||k==0||i==maxh-1||k==maxw-1){
-					x[i,k] = 1;
-				}
-				else{
-					x[i,k] = 0;
-				}
-				
-				
-			
-			
-		}
-		for(int i= 2; i<maxh-2; i=i+2)
-			for(int k= 2; k<maxw-2; k=k+2){
-				if(y==0){
-					x[i,k] = 1;
-					y = 1;
-			}
-			else{
-				y--;
-			}
-			
-		}
-		for(int i= 0; i<=maxh-1; i++)
-			for(int k= 0; k<=maxw-1; k++){
-				if(x[i,k] != 0 && x[i,k] !=1){
-				if(Random.Range(0,2)==1){
-					x[i,k] = 2;
-				}
-				
-					
-			}
-					
-			
-			
-		}
-		for(int i= 0; i<=maxh-1; i++)
-			for(int k= 0; k<=maxw-1; k++){
-		
-		switch (x[i,k]){
-			
-		case 0:
-			Instantiate(ragingboner,new Vector3(i,0,k),Quaternion.identity);
-				if(((k == 1 && i == 1)||(k == 2 && i == 1)||(k == 1 && i == 2))||((k == maxw-2 && i == maxh-2)||(k == maxw-3 && i == maxh-2)||(k == maxw-2 && i == maxh-3))||((k == maxw-2 && i == 1)||(k == maxw-3 && i == 1)||(k == maxw-2 && i == 2))||((k == 1 && i == maxh-2)||(k == 2 && i == maxh-2)||(k == 1 && i == maxh-3))){
-					break;
-				}else{
-				if(Random.Range(0,3)>=1){
-				Instantiate(crate,new Vector3(i,1,k),Quaternion.identity);	
-					}	
-				}
-			break;
-		case 1:
-			Instantiate(wall,new Vector3(i,1,k),Quaternion.identity);
-			break;
-		case 2:
-			Instantiate(crate,new Vector3(i,1,k),Quaternion.identity);
-			break;
-		default:
-			break;
-			
-			
-			
-			}
-		switch (spawnnumber){
-			case 0:
-				Instantiate(player1,new Vector3(1,1,1),Quaternion.identity);
-				Instantiate(player2,new Vector3(maxh-2,1,maxw-2),Quaternion.identity);
-				break;
-			case 1:
-				Instantiate(player1,new Vector3(maxh-2,1,1),Quaternion.identity);
-				Instantiate(player2,new Vector3(1,1,maxw-2),Quaternion.identity);
-				break;
-			case 2:
-				Instantiate(player1,new Vector3(1,1,maxw-2),Quaternion.identity);
-				Instantiate(player2,new Vector3(maxh-2,1,1),Quaternion.identity);
-				break;
-			case 3:
-				Instantiate(player2,new Vector3(1,1,1),Quaternion.identity);
-				Instantiate(player1,new Vector3(maxh-2,1,maxw-2),Quaternion.identity);
-				break;
-			default:
-				break;				
-			}
-				
-				
-			
-		}
-		
-		
-		
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+        //Spawn Floor
+        Transform Floor = (Transform)Instantiate(ragingboner, new Vector3(maxw/2, 0, maxh/2), Quaternion.identity);
+        Floor.transform.localScale = new Vector3(maxw, 1, maxh);
+        //Generate seed value to determine the spawn of the players
+        spawnnumber = Random.Range(0, 4);
+
+        switch (spawnnumber)
+        {
+            case 0:
+                Instantiate(player1, new Vector3(1, 1, 1), Quaternion.identity);
+                Instantiate(player2, new Vector3(maxh - 2, 1, maxw - 2), Quaternion.identity);
+                break;
+            case 1:
+                Instantiate(player1, new Vector3(maxh - 2, 1, 1), Quaternion.identity);
+                Instantiate(player2, new Vector3(1, 1, maxw - 2), Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(player1, new Vector3(1, 1, maxw - 2), Quaternion.identity);
+                Instantiate(player2, new Vector3(maxh - 2, 1, 1), Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(player2, new Vector3(1, 1, 1), Quaternion.identity);
+                Instantiate(player1, new Vector3(maxh - 2, 1, maxw - 2), Quaternion.identity);
+                break;
+            default:
+                break;
+        }
+
+        //Camera moves accordingly with field size to compensate for bigger areas
+        camera.transform.position = new Vector3(maxh / 2, maxh + 5, maxw / 2);
+
+
+        //LEVEL GENERATION
+        x = new int[maxh, maxw];
+        for (int i = 0; i <= maxh - 1; i++)
+            for (int k = 0; k <= maxw - 1; k++)
+            {
+                if (i == 0 || k == 0 || i == maxh - 1 || k == maxw - 1)
+                {
+                    x[i, k] = 1;
+                }
+                else
+                {
+                    x[i, k] = 0;
+                }
+
+
+
+
+            }
+        for (int i = 2; i < maxh - 2; i = i + 2)
+            for (int k = 2; k < maxw - 2; k = k + 2)
+            {
+                if (y == 0)
+                {
+                    x[i, k] = 1;
+                    y = 1;
+                }
+                else
+                {
+                    y--;
+                }
+
+            }
+        for (int i = 0; i <= maxh - 1; i++)
+            for (int k = 0; k <= maxw - 1; k++)
+            {
+                if (x[i, k] != 0 && x[i, k] != 1)
+                {
+                    if (Random.Range(0, 2) == 1)
+                    {
+                        x[i, k] = 2;
+                    }
+
+
+                }
+
+
+
+            }
+        //Spawn boxes loop
+        for (int i = 0; i <= maxh - 1; i++)
+            for (int k = 0; k <= maxw - 1; k++)
+            {
+
+
+                switch (x[i, k])
+                {
+
+                    case 0:
+                        if (((k == 1 && i == 1) || (k == 2 && i == 1) || (k == 1 && i == 2)) || ((k == maxw - 2 && i == maxh - 2) || (k == maxw - 3 && i == maxh - 2) || (k == maxw - 2 && i == maxh - 3)) || ((k == maxw - 2 && i == 1) || (k == maxw - 3 && i == 1) || (k == maxw - 2 && i == 2)) || ((k == 1 && i == maxh - 2) || (k == 2 && i == maxh - 2) || (k == 1 && i == maxh - 3)))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            //30% chance to spawn a destructible block on top of the floor 
+                            if (Random.Range(0, 3) >= 1)
+                            {
+                                Instantiate(crate, new Vector3(i, 1, k), Quaternion.identity);
+                            }
+                        }
+                        break;
+                    case 1:
+                        Instantiate(wall, new Vector3(i, 1, k), Quaternion.identity);
+                        break;
+                    case 2:
+                        Instantiate(crate, new Vector3(i, 1, k), Quaternion.identity);
+                        break;
+                    default:
+                        break;
+                }
+            }
+    }
 }
+
+	
