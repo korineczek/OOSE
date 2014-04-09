0e6e1fe838d36710705bed5664689872a9869fbd
RasmusKlaustrup <fifarallebusiness@gmail.com>
2014-04-09 10:03:03 +0200

    RayCasting Walls
    Raycasting applied to movement script to detect walls. Still need a bit
    of improvement

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class Movement : MonoBehaviour
 {
     public float speed = 1.0f;
-
     public Vector3 lastPosition;
     public Vector3 endpos;
     public bool moving = false;
+	private bool objectHitUp;
+	private bool objectHitDown;
+	private bool objectHitRight;
+	private bool objectHitLeft;
+	RaycastHit hit;
     
 
     void Start()
@@ -25,38 +29,84 @@ public class Movement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, endpos, Time.deltaTime * speed);
     }
 
-    void OnCollisionEnter(Collision other)
-    {
-
-        /*	if (other.gameObject.tag != "DestructableCube") 
-                lastPosition = transform.position;
-    */
-
-        if (other.gameObject.tag == "DestructableCube")
-            transform.position = endpos;
-    }
    public void moveUp()
     {
-        moving = true;
-        endpos = transform.position + Vector3.forward;
-
-    }
+		rayCast ();
+		if (objectHitUp == false) {
+						moving = true;
+						endpos = transform.position + Vector3.forward;
+		}
+	}
     public void moveDown()
     {
-        moving = true;
-        endpos = transform.position + Vector3.back;
-
+		
+		rayCast ();
+		if (objectHitDown == false) {
+						moving = true;
+						endpos = transform.position + Vector3.back;
+				}
     }
     public void moveLeft()
     {
-        moving = true;
-        endpos = transform.position + Vector3.left;
-
+		rayCast ();
+		if (objectHitLeft == false) {
+						moving = true;
+						endpos = transform.position + Vector3.left;
+				}
     }
     public void moveRight()
     {
-        moving = true;
-        endpos = transform.position + Vector3.right;
-
+		
+		rayCast ();
+		if (objectHitRight == false) {
+						moving = true;
+						endpos = transform.position + Vector3.right;
+				}
     }
+	public void rayCast()
+	{
+		Vector3 up = transform.TransformDirection (Vector3.forward);
+		Vector3 down = transform.TransformDirection (Vector3.back);
+		Vector3 right = transform.TransformDirection (Vector3.right);
+		Vector3 left = transform.TransformDirection (Vector3.left);
+		
+		if (Physics.Raycast (endpos, up, out hit)) {
+			if (hit.collider.gameObject.tag == "Wall"&& hit.distance<1.0) {
+				Debug.Log ("HitUp"+ hit.distance);
+				objectHitUp = true;
+			}
+		} else
+			noObjectHit ();
+		
+		if (Physics.Raycast (endpos, down, out hit)) {
+			if (hit.collider.gameObject.tag == "Wall" && hit.distance<1.0) {
+				Debug.Log ("HitDown" + hit.distance);
+				objectHitDown = true;
+			}
+		} else
+			noObjectHit ();
+		
+		if (Physics.Raycast (endpos, right, out hit)) {
+			if (hit.collider.gameObject.tag == "Wall" && hit.distance<1.0) {
+				Debug.Log ("HitRight"+ hit.distance);
+				objectHitRight = true;
+			}
+		} else
+			noObjectHit ();	
+		
+		if (Physics.Raycast (endpos, left, out hit)) {
+			if (hit.collider.gameObject.tag == "Wall" && hit.distance<1.0) {
+				Debug.Log ("HitLeft"+ hit.distance);
+				objectHitLeft = true;
+			}
+		} else
+			noObjectHit ();
+	}
+	public void noObjectHit(){
+		objectHitUp = false;
+		objectHitDown = false;
+		objectHitLeft = false;
+		objectHitRight = false;
+		
+	}
 }
