254bcef19cec3d24328d467458dad73bb1ba1ff6
RasmusKlaustrup <fifarallebusiness@gmail.com>
2014-04-25 15:17:26 +0200

    Gui Fixed and Bombs

---
+++ Assets/scripts/Gui.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gui : MonoBehaviour {
+
+    int rangeCount;
+    int bombCount;
+
+	// Use this for initialization
+	void Start () {
+ 	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rangeCount = GameObject.FindWithTag("Player1").GetComponent<characterBehaviour>().rangeCount;
+        bombCount = GameObject.FindWithTag("Player1").GetComponent<characterBehaviour>().bombCount;
+
+        OnGUI();
+       // RangeGUI();
+                
+	}
+
+
+    //Functon for Bomb GUI
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Bombs: "+ bombCount.ToString());
+        int actualRange = rangeCount - 1;
+        GUI.Label(new Rect(10, 30, 100, 30), "Bomb Range: " + actualRange.ToString());
+    }
+}
--- Assets/scripts/characterBehaviour.cs
+++ Assets/scripts/characterBehaviour.cs
@@ -3,18 +3,15 @@ using System.Collections;
 
 public class characterBehaviour : MonoBehaviour {
 
-	public GUIText bombCountText;
+	//public GUIText bombCountText;
 	public int bombCount=1;
-	public GUIText rangeCounterText;
+	//public GUIText rangeCounterText;
 	public int rangeCount=2;
 	public Vector3 tempPos;
 	public bool test1;
 
 	public Rigidbody bombPrefab;
-	//public GameObject explosionPrefab;
-  //  private GameObject cloneUp, cloneDown, cloneRight, cloneLeft;
-   // private bool test, test1, test2, test3;
-
+	
 	void start()
 	{
 
@@ -23,13 +20,6 @@ public class characterBehaviour : MonoBehaviour {
 
 	void Update()
 	{
-		//triggerBomb ();
-		/*cloneDown.GetComponent<explosionScript>().explodeDown();
-		cloneUp.GetComponent<explosionScript>().explodeUp();
-		cloneRight.GetComponent<explosionScript>().explodeRight();
-		cloneLeft.GetComponent<explosionScript>().explodeLeft();
-        */
-	
 	}
 
 
@@ -50,14 +40,10 @@ public class characterBehaviour : MonoBehaviour {
 		Instantiate (bombPrefab, transform.position, transform.rotation);
 		tempPos = transform.position;
 		bombCount = bombCount -1;
-		SetBombCountText();
-		yield return new WaitForSeconds(3);
-		/*cloneUp = Instantiate (explosionPrefab, tempPos + new Vector3 (0, 0, 1), bombPrefab.rotation) as GameObject;
-		cloneDown = Instantiate (explosionPrefab, tempPos + new Vector3 (0, 0, -1), bombPrefab.rotation) as GameObject;
-		cloneRight = Instantiate (explosionPrefab, tempPos + new Vector3 (1, 0, 0), bombPrefab.rotation) as GameObject;
-		cloneLeft = Instantiate (explosionPrefab, tempPos + new Vector3 (-1, 0, 0), bombPrefab.rotation) as GameObject;*/
+		//SetBombCountText();
+		yield return new WaitForSeconds(12);
 		bombCount = bombCount+1;
-		SetBombCountText();
+		//SetBombCountText();
 
 	}
 
@@ -67,38 +53,13 @@ public class characterBehaviour : MonoBehaviour {
 				if (other.gameObject.tag == "BombPickup") {
 						Destroy (other.gameObject);
 						bombCount = bombCount + 1;
-						SetBombCountText ();
+						//SetBombCountText ();
 				}
 				if (other.gameObject.tag == "RangePickup") {
 						Destroy (other.gameObject);
 						rangeCount = rangeCount + 1;
-						SetRangeCountText ();
+						//SetRangeCountText ();
 				}
-	}
-
-	void test()
-	{
-						
-						
-			
-		}
-
-	//Functon for Bomb GUI
-	void SetBombCountText()
-	{
-		Debug.Log("called");
-		bombCountText.text = "Bombs: " + bombCount.ToString();
-	}
-
-	//Functoin for Range GUI
-	void SetRangeCountText()
-	{
-		int actualRange = rangeCount - 1;
-		rangeCounterText.text = "Bomb Range: " + actualRange.ToString();
-	}
-	void OnGUI(){
-		bombCountText.text = "Bombs: " + bombCount.ToString();
-		SetRangeCountText();
 	}
 
 }
