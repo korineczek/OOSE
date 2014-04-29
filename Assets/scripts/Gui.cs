3a88b98d04f35312a00e76b22a651fb80da9bfec
RasmusKlaustrup <fifarallebusiness@gmail.com>
2014-04-29 12:24:01 +0200

    PlayerCount Complete

--- Assets/scripts/Gui.cs
+++ Assets/scripts/Gui.cs
@@ -7,6 +7,8 @@ public class Gui : MonoBehaviour {
     int bombCount1;
     int rangeCount2;
     int bombCount2;
+    int red;
+    int blue;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +19,8 @@ public class Gui : MonoBehaviour {
 
         rangeCount1 = GameObject.FindWithTag("Player1").GetComponent<characterBehaviour>().rangeCount;
         bombCount1 = GameObject.FindWithTag("Player1").GetComponent<characterBehaviour>().bombCount;
+        red = GameObject.FindWithTag("Handler").GetComponent<IRChandler>().redplayers;
+        blue = GameObject.FindWithTag("Handler").GetComponent<IRChandler>().blueplayers;
 
         rangeCount2 = GameObject.FindWithTag("Player2").GetComponent<characterBehaviour>().rangeCount;
         bombCount2 = GameObject.FindWithTag("Player2").GetComponent<characterBehaviour>().bombCount;
@@ -32,9 +36,13 @@ public class Gui : MonoBehaviour {
         GUI.Label(new Rect(10, 10, 100, 20), "Bombs: "+ bombCount1.ToString());
         int actualRange1 = rangeCount1 - 1;
         GUI.Label(new Rect(10, 30, 100, 30), "Bomb Range: " + actualRange1.ToString());
+        GUI.Label(new Rect(10, 50, 200, 30), "Red Players: " + red.ToString());
+
 
         GUI.Label(new Rect(700, 10, 100, 20), "Bombs: " + bombCount2.ToString());
         int actualRange2 = rangeCount2 - 1;
         GUI.Label(new Rect(700, 30, 100, 30), "Bomb Range: " + actualRange2.ToString());
+        GUI.Label(new Rect(700, 50, 200, 30), "Blue Players: " + blue.ToString());
+
     }
 }
